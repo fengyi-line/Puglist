a8d3bb52dfb104dc3fb665828bf854f753592bbe
Tony Tsai <fengyi.tsai@linecorp.com>
2019-08-28 15:50:25 +0900

    Initial Commit

---
+++ puglist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  puglist
+//
+//  Created by ST20991 on 2019/08/28.
+//  Copyright © 2019 fengyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
